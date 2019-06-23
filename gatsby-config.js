b03aa002862443db497f2612ae3f439fe9349867
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-06-23 15:46:30 -0300

    Removing Contentful plugins and unnecessary configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,5 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-netlify`,
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
   ],
 }
